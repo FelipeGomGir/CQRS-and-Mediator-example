27a59f44eb3c3cd1ceddaf27411b22502c7b84cd
Felipe Gómez <fgomez@lwolf.com>
2025-08-28 10:49:16 -0500

    Clean Arquitecture Course

---
+++ HR.LEaveManagement.Domain/LeaveAllocation.cs
@@ -0,0 +1,12 @@
+﻿using HR.LeaveManagement.Domain.Common;
+
+namespace HR.LeaveManagement.Domain;
+
+public class LeaveAllocation: BaseEntity
+{
+    public int Id { get; set; }
+    public string NumberOfDays { get; set; }
+    public LeaveType? LeaveType { get; set; } /// This is a foreing key between LeaveType and Leave Allocation 
+    public int LeaveTypeId { get; set; } /// This is a foreing key between LeaveType and Leave Allocation 
+    public int Period { get; set; }
+}
