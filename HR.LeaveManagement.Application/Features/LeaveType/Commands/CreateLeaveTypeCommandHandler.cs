64022a7db44ce19de26518e6d9c1cc32769195ed
FelipeGomGir <fgomez@lwolf.com>
2025-08-31 22:44:14 -0500

    Creating first queries and commands

---
+++ HR.LeaveManagement.Application/Features/LeaveType/Commands/CreateLeaveTypeCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Commands
+{
+    public class CreateLeaveTypeCommandHandler : IRequestHandler<CreateLeaveTypeCommand, int>
+    {
+        private readonly IMapper _mapper;
+        private readonly ILeaveTypeRepository _repo;
+
+        public CreateLeaveTypeCommandHandler(IMapper mapper, ILeaveTypeRepository repo)
+        {
+            this._mapper = mapper;
+            this._repo = repo;
+        }
+        public async Task<int> Handle(CreateLeaveTypeCommand request, CancellationToken cancellationToken)
+        {
+            // validate incoming data
+
+            // Convert to domain entity type object
+            var LeaveTypeToCreate = _mapper.Map<Domain.LeaveType>(request);
+
+            // add to database 
+            await _repo.CreateAsync(LeaveTypeToCreate);
+
+            //return record id
+            return LeaveTypeToCreate.Id;
+        }
+    }
+}
---
+++ HR.LeaveManagement.Application/Features/LeaveType/Queries/GetLeaveTypeDetails/GetLeaveTypeDetailsHandler.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Queries.GetLeaveTypeDetails
+{
+    public class GetLeaveTypeDetailsHandler : IRequestHandler<GetLeaveTypesDetailsQuery, LeaveTypeDetailsDto>
+    {
+        private readonly IMapper _mapper;
+        private readonly ILeaveTypeRepository _repo;
+
+        public GetLeaveTypeDetailsHandler(IMapper mapper, ILeaveTypeRepository repo)
+        {
+            this._mapper = mapper;
+            this._repo = repo;
+        }
+
+        public async Task<List<LeaveTypeDetailsDto>> Handle(GetLeaveTypesDetailsQuery request, CancellationToken cancellationToken)
+        {
+            // Query to database
+            var LeaveTypesDetail = await _repo.GetByIdAsync(request.Id);
+
+            // Convert from entity to Dto 
+            var data = _mapper.Map<List<LeaveTypeDetailsDto>>(LeaveTypesDetail);
+            //_mapper.Map<TDestination>(source);
+
+            //return Dto
+            return data;
+        }
+    }
+}
